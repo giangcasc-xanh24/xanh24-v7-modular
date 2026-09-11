4ff374a18316fadbe81e6f60e2bdd39b3b31365f
giangcasc-xanh24 <giang.casc@gmail.com>
2026-09-11 10:31:05 +0700

    Add files via upload

---
+++ apps/web/next.config.js
@@ -0,0 +1,8 @@
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: { ignoreBuildErrors: true },
+  eslint: { ignoreDuringBuilds: true },
+  experimental: { esmExternals: true }
+}
+module.exports = nextConfig
